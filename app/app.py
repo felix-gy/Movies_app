eb43f067adc5c8cfefaf4911c50ffaeedc46e1c4
FelixG <felix.gordillo@ucsp.edu.pe>
2023-12-09 11:33:08 -0500

    update user.py and routes login

--- app/app.py
+++ app/app.py
@@ -1,13 +1,46 @@
 from flask import Flask, render_template, request, redirect,url_for
 from queries.movies import *
+from queries.users import *
 
 app = Flask(__name__)
 
+my_session = {'logged':False}
 
 @app.route('/')
 def index():
     peliculas = all_movies()
     return render_template('BDII_index.html', peliculas=peliculas)
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/submit_register', methods=['POST'])
+def submit_register():
+    nombre = request.form['nombre']
+    email = request.form['email']
+    contraseña = request.form['password']
+    insert_user(nombre,email,contraseña)
+    return 'Formulario enviado correctamente - register '
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/submit_login', methods=['POST'])
+def submit_login():
+    email = request.form['email']
+    contraseña = request.form['password']
+    id_user = get_user_id(email)
+    if id_user:
+        if verify_password(id_user, contraseña):
+            my_session['logged'] = True
+            my_session['id'] = id_user
+            return redirect(url_for('index'))
+        else:
+            return redirect(url_for('login'))
+    else:
+        return redirect(url_for('login'))
 
 # Ruta para redirigir a Flask usando el ID de la película
 @app.route('/ver_pelicula/<uuid>')
